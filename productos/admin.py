61f2cabb70809338fda6bde8fcc069849597a03b
Javi Toledo <javier.toledo1713@gmail.com>
2026-07-23 03:32:05 -0300

    feat: crear modelo categoria y configuracion admin

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Categoria
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = (
+        "nombre",
+        "disponible",
+    )
+
+    ordering = ("nombre",)
+
 
 # Register your models here.
